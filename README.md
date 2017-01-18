88d1e38db0042a4c9d2f353c2e0fbb0c41f37c59
Thomas <thomas@pydio.com>
2017-01-18 16:10:00 +0100

    Could this be a one commit thing?

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # python-simple-upload
 SimpleHTTP POST upload script - LAN Transfer files from any device with a web browser
+
+Python script to serve and upload files. I use it to transfer files from my phone to my computer.
+Totally unsafe:
+    - no transport security (TLS, HTTPS)
+    - no filename sanitization
+    - no checks of any kind
+
+This is hugly but kind of self contained.
+
+# How to run
+
+	python pyup.py
+
---
+++ pyup.py
@@ -0,0 +1,65 @@
+import os, socket
+from flask import Flask, request, send_from_directory
+
+# Not meant to be long-term persisted
+UPLOAD_FOLDER = '/tmp/uploads'
+# Hope port is not used
+PORT = 8080
+# Try to figure out the IP
+HOST = [(s.connect(('192.168.0.0', 80)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]
+
+app = Flask(__name__)
+# Increase max file upload size just in case
+app.config['MAX_CONTENT_LENGTH'] = 1000 * 1024 * 1024 # MB max size
+
+# configure upload folder
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+UPLOAD_FORM = '''
+        <h1>Insecure file upload</h1>
+        <form action=upload method=post enctype=multipart/form-data>
+            <input type=file name=file>
+            <input type=submit value=Upload>
+        </form>
+'''
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return 'No file present'
+        f = request.files['file']
+        if f.filename == '':
+            return 'No file name'
+        f.save(os.path.join(app.config['UPLOAD_FOLDER'], f.filename))
+        return 'Successfully Uploaded'
+    else:
+        return '''
+            <!doctype html>
+            <head><title>Insecure simple file upload</title></head>
+        ''' + UPLOAD_FORM
+
+@app.route('/', methods=['GET'])
+def index():
+    '''
+        Super basic listing of files
+    '''
+    resp = UPLOAD_FORM
+    resp += '<div><h1>List of files in ' + UPLOAD_FOLDER + '</h1>'
+    resp += '<ol>'
+    for f in os.listdir(UPLOAD_FOLDER):
+        resp += '<li>'
+        resp += '<a href="' + os.path.join(UPLOAD_FOLDER, f) + '">' + f + '</a>'
+        resp += '</li>'
+    resp += '</div>'
+    return resp
+
+@app.route(UPLOAD_FOLDER + '/<path:filename>', methods=['GET'])
+def serve_files(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+# run the app
+app.run(port=PORT, host=HOST, debug=False)
